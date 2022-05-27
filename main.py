8ad120528ba3e07bed2c582b4ef82771ea311343
atulg07 <atulgavali7@gmail.com>
2022-05-27 18:44:12 +0530

    flask file and index html file created

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+
+from crypt import methods
+from pdb import post_mortem
 import numpy as np
-from flask import Flask
+from flask import Flask,request,render_template
 import pandas as pd 
+
+import pickle
+
+
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return "API DEFAULT"
+
+@app.route("/data",methods=["GET","POST"])
+def data():
+    user=request.form
+    print(user)
+
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
